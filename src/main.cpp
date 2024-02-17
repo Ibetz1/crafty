14b83d55e84813f7f656b80535b67935e54b46f1
Caleb Barger <bargercj20@hotmail.com>
2024-02-17 11:43:59 -0800

    Draw a cube and fly around.

--- src/main.cpp
+++ src/main.cpp
@@ -1,47 +1,75 @@
 #include "raylib.h"
+#include "raymath.h"
 
 extern "C" {
     #include "base_inc.h"
     #include "base_inc.c"
 }
 
+global Camera global_camera = { 0 };
+
+global Mesh global_DEBUG_block_mesh; 
+global Material global_DEBUG_block_material;
+global Model global_DEBUG_block_model;
+
 /*
     on runtime
 */
-void init() {
-
+internal void init() {
+    const int screen_width = 800;
+    const int screen_height = 450;
+    InitWindow(screen_width, screen_height, "AlgoCraft3D");
+    
+    global_DEBUG_block_mesh = GenMeshCube(1.0f, 1.0f, 1.0f);                            
+    global_DEBUG_block_material = LoadMaterialDefault();
+    global_DEBUG_block_model = LoadModelFromMesh(global_DEBUG_block_mesh);
+    
+    //- NOTE(cabarger): camera default values...
+    global_camera.position = (Vector3){ 0.2f, 0.4f, 0.2f }; 
+    global_camera.target = (Vector3){ 0.185f, 0.4f, 0.0f }; // Looking at point
+    global_camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };       // Up vector (rotation towards target)
+    global_camera.fovy = 45.0f;                             // Field-of-view Y
+    global_camera.projection = CAMERA_PERSPECTIVE;          // Projection type
+    Vector3 position = { 0.0f, 0.0f, 0.0f };                // Set model position
 }
 
 /*
     pre-render pass
 */
-void update(float dt) {
-    
+internal void update(F32 dt) {
+    UpdateCamera(&global_camera, CAMERA_FIRST_PERSON);
 }
 
 /*
     render pass
 */
-void draw() {
+internal void draw() {
+    BeginDrawing();
+    ClearBackground(BLACK);
 
+    BeginMode3D(global_camera);
+
+    DrawModel(
+        global_DEBUG_block_model, 
+        (Vector3){.x = 0.0f, .y = 0.0f, .z = 0.0f},  // Pos
+        1.0f, // Scale
+        WHITE // Tint
+    ); 
+
+    EndMode3D();
+   EndDrawing();
 }
 
 int main(void) {
+
+
     init();
-
-    const int screenWidth = 800;
-    const int screenHeight = 450;
-
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
     SetTargetFPS(60);
-    init();
+    DisableCursor();                // Limit cursor to relative movement inside the window
 
     while (!WindowShouldClose()) {
         update(GetFrameTime());
-        BeginDrawing();
-            ClearBackground(RAYWHITE);
-            draw();
-        EndDrawing();
+        draw();
     }
     
     CloseWindow();
