642fe84c6b6b4987b3a604ba44c10acd1145019b
Samuel Beal <62578556+spbeal@users.noreply.github.com>
2024-02-18 09:11:28 -0800

    UI refactor

--- include/ui.hpp
+++ include/ui.hpp
@@ -6,8 +6,6 @@ void create_ui();
 void update_hotbar(int screen_width, int screen_height);
 void draw_hotbar(int screen_width, int screen_height);
 
-
-
 struct Button
 {
 	int x;
--- src/ui.cpp
+++ src/ui.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 
 std::vector <Button> buttons;
-int highlighted = 0;
+int highlighted_square = 0;
 
 void create_button(int x, int y, int width, int height)
 {
@@ -26,6 +26,7 @@ void update_hotbar(int screen_width, int screen_height)
 {
 	for (int i = 0; i < 9; i++)
 	{
+		// Proportional dimensions to screen height & width
 		buttons[i].x = screen_width/8+(i*((int)screen_width/12));
 		buttons[i].y = screen_height-(screen_width/12)-(screen_height/60);
 		buttons[i].width = screen_width/12;
@@ -38,13 +39,13 @@ void update_hotbar(int screen_width, int screen_height)
 		// -1 for down, 1 for up
 		if (mouseWheelMovement == -1)
 		{
-			if (highlighted < 8) highlighted++;
-			else highlighted = 0;
+			if (highlighted_square < 8) highlighted_square++;
+			else highlighted_square = 0;
 		}
 		else
 		{
-			if (highlighted > 0) highlighted--;
-			else highlighted = 8;
+			if (highlighted_square > 0) highlighted_square--;
+			else highlighted_square = 8;
 
 		}
  	}
@@ -54,7 +55,7 @@ void update_hotbar(int screen_width, int screen_height)
 		// KEY_ONE starts at index 49
 		if (IsKeyDown(i+49))
 		{
-			highlighted = i;
+			highlighted_square = i;
 		}
 	}
 
@@ -66,8 +67,7 @@ void draw_hotbar(int screen_width, int screen_height)
 	for (const Button& button : buttons)
 	{
 		DrawRectangle(button.x, button.y, button.width, button.height, Fade(LIGHTGRAY, 0.3f));
-
-		if (i == highlighted)
+		if (i == highlighted_square)
 		{
 			// top bottom left right
 			DrawLine(button.x, button.y, button.x+button.width, button.y, LIGHTGRAY);
@@ -77,9 +77,8 @@ void draw_hotbar(int screen_width, int screen_height)
 		}
 		else
 		{
-			// left
+			// draw the first left then draw the top bottom and right lines to the end of the hotbar
 			if (i == 0) DrawLine(button.x, button.y, button.x, button.y+button.height, GRAY);
-			// Draw the rest of a square (top, bottom, right)
 			DrawLine(button.x+button.width, button.y, button.x+button.width, button.y+button.height, GRAY);
 			DrawLine(button.x, button.y, button.x+button.width, button.y, GRAY);
 			DrawLine(button.x, button.y+button.height, button.x+button.width, button.y+button.height, GRAY);
